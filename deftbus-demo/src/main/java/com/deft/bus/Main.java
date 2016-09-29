3e5ecb492c3afaba99eb8ee849590f3fd87a4c82
wei <wei2006004@foxmail.com>
2016-09-29 18:02:10 +0800

    demo init

---
+++ deftbus-demo/src/main/java/com/deft/bus/Main.java
@@ -0,0 +1,10 @@
+package com.deft.bus;
+
+/**
+ * Created by Administrator on 2016/9/29.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
